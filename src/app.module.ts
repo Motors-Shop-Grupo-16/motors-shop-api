0926cc3ad2620960628611c4932df76e734c1704
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-02-22 16:17:20 -0300

    feat: add login route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './routes/users/users.module';
+import { LoginModule } from './routes/login/login.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, LoginModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
