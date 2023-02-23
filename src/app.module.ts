be95ecf279029c2f9163d2e1d60c3ce02da6351c
Leandro Schillreff <leandroschillreff@gmail.com>
2023-02-23 10:43:38 -0400

    fix: announcement routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ensureAuthMiddleware } from './middlewares/ensureAuth.middleware';
 import { UsersController } from './routes/users/users.controller';
 import { AnnouncementModule } from './routes/announcement/announcement.module';
 import { AnnouncementController } from './routes/announcement/announcement.controller';
-import { isAdvertiser } from './middlewares/isAdvertiser.middleware';
+import { ensureIsAdvertiser } from './middlewares/ensureIsAdvertiser.middleware';
 
 @Module({
   imports: [UsersModule, LoginModule, AnnouncementModule],
@@ -16,14 +16,17 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(ensureAuthMiddleware)
-      .exclude({ path: 'users', method: RequestMethod.POST })
-      .forRoutes(UsersController);
-    consumer
-      .apply(ensureAuthMiddleware, isAdvertiser)
       .exclude(
+        { path: 'users', method: RequestMethod.POST },
         { path: 'announcements', method: RequestMethod.GET },
         { path: 'announcements/notAdvertiser/:id', method: RequestMethod.GET },
       )
-      .forRoutes(AnnouncementController);
+      .forRoutes(UsersController, AnnouncementController);
+    consumer
+      .apply(ensureIsAdvertiser)
+      .forRoutes(
+        { path: 'announcements', method: RequestMethod.POST },
+        'announcements/advertiser/',
+      );
   }
 }
