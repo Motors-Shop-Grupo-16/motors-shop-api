0f1eef2fb8b59027e3675438aea7bfa61b91ea1a
Leandro Schillreff <leandroschillreff@gmail.com>
2023-03-06 20:35:20 -0400

    fix: deploy

--- src/routes/comments/comments.module.ts
+++ src/routes/comments/comments.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { CommentController } from './comments.controller';
 import { CommentsService } from './comments.service';
 
